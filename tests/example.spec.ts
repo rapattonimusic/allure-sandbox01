3e8ab5af632a1dad8988809b8e3b5fa38bdf8054
rapattonimusic <36390452+rapattonimusic@users.noreply.github.com>
2023-08-23 09:23:01 -0700

    Adding tests.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('A test that will pass', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('Another test that will pass', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -16,3 +16,7 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('A test that will fail', async ({ page }) => {
+  expect('2').toBe('1');
+})
