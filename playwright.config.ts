9c2992370d48155f556cbf7ed1a74223491d8e82
rapattonimusic <36390452+rapattonimusic@users.noreply.github.com>
2023-08-23 06:22:03 -0700

    Removing a line

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,7 +68,6 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
   //   url: 'http://127.0.0.1:3000',
